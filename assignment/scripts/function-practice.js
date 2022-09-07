bc93fbd6e45de6650ce5f01e6e00b05cb1ccd35d
Astrid Pulse <astridcpulse@gmail.com>
2022-09-07 11:38:06 -0500

    wrote and tested the loop for problem 7 successfully

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -65,8 +65,15 @@ console.log( 'getLast', getLast(myArray));
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  for ( let i = 0; i < array.length; i++){
+    if (array[i] === value){
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log( 'find', find( 'Terrence Badswell', myArray));
 
 // ----------------------
 // Stretch Goals
